d6500f3006460ef955596304c3ac56dc4b1fe6c7
heriet <heriet@heriet.info>
2017-10-24 00:29:41 +0900

    add import computing sub modules

--- sleety/computing/__init__.py
+++ sleety/computing/__init__.py
@@ -1,1 +1,4 @@
 from .connection import ComputingConnection, ComputingResponse  # noqa: F401
+from . import instance  # noqa: F401
+from . import fw  # noqa: F401
+from . import region  # noqa: F401
